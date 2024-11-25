d71fcbc38e67d5a826dc3e35bbd34e1a64f2126e
Chetan Wani <150382947+Wani-Chetan-999@users.noreply.github.com>
2024-11-25 22:33:29 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -125,7 +125,7 @@ function showProjects(projects) {
     projects.slice(0, 10).filter(project => project.category != "android").forEach(project => {
         projectHTML += `
     <div class="box tilt">
-      <img draggable="false" src="/personalportfolio/assets/images/projects/${project.image}.png" alt="project 1" />
+      <img draggable="false" src="./assets/images/projects/${project.image}.png" alt="project 1" />
       <div class="content">
         <div class="tag">
         <h3>${project.name}</h3>
